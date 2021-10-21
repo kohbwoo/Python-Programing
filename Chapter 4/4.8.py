5afb2291ca204aa18f67decef58e13d5947b8c24
ByungWoo Koh <kohbwoo@gmail.com>
2021-10-21 23:56:03 +0900

    commit

--- Chapter 4/4.8.py
+++ Chapter 4/4.8.py
@@ -1,2 +1,18 @@
-for i in range(1,2,3,4):
-    print()
+def mean6(a, b, c, d, e, f):
+    tmp = [a, b, c, d, e, f]
+    return sum(tmp)/6
+
+
+def max6(a, b, c, d, e, f):
+    return max(a, b, c, d, e, f)
+
+
+def min6(a, b, c, d, e, f):
+    return min(a, b, c, d, e, f)
+
+
+a, b, c, d, e, f = input('여섯 개의 수를 입력하시오. : ').split()
+a, b, c, d, e, f = int(a), int(b), int(c), int(d), int(e), int(f)
+print(a, b, c, d, e, f, "의 평균값은", mean6(a, b, c, d, e, f))
+print(a, b, c, d, e, f, "의 최대값은", max6(a, b, c, d, e, f))
+print(a, b, c, d, e, f, "의 최소값은", min6(a, b, c, d, e, f))
--- Test.py
+++ Test.py
@@ -1,26 +1,2 @@
-from sklearn.datasets import load_breast_cancer
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import matplotlib
-
-# matplotlib 설정
-matplotlib.rc('font', family='AppleGothic') # 한글 설정
-plt.rcParams['axes.unicode_minus'] = False # -표시
-
-# data load
-cancer = load_breast_cancer()
-
-# data  분할
-x_train, x_test, y_train, y_test = \
-  train_test_split( # 데이터 분할을 위해
-      cancer.data, cancer.target, # 분할할 데이터
-      random_state=0, test_size=0.3 # 랜덤상태, 테스트 비율
-      )
-
-# feature visualization
-plt.boxplot(x_train, manage_xticks=False) # 데이터, 소눈금 표시 안하기
-plt.yscale('symlog') # 축 스케일을 log 로
-plt.xlabel('feature list') # x축 이름
-plt.ylabel('feature') # y축 이름
-plt.show() # 그래프 출력
+a = [11,56,2,2,43,124]
+print(sum(sorted(a,reverse=True)[:3]))
