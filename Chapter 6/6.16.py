2843b8b48814a5dcbf7e23efc4ea6e38051ed1eb
kohbwoo <kohbwoo@gmail.com>
2021-11-07 19:48:44 +0900

    6장 연습문제 풀이

--- Chapter 6/6.16.py
+++ Chapter 6/6.16.py
@@ -1,9 +1,9 @@
 student_tuple = (("191101", "홍길동", "010-123-45xx"), ("191102", "임꺽정", "010-223-45xx"), ("191103", "장길산", "010-323-45xx"))
-
-tmp = [n for n,x in enumerate(student_tuple) if x=='191101']
-print(tmp)
-
-
-# while True:
-#     tmp = input("학번을 입력하세요 :")
-#     print(tmp,"번 학생은", student_tuple)
+while True:
+    tmp = input("학번을 입력하세요 :")
+    if tmp == "-1": break
+    for i in student_tuple:
+        if i[0] == tmp:
+            ans = i[1]
+    print(tmp, "번 학생은", ans, "입니다.")
+print("프로그램을 종료합니다.")
