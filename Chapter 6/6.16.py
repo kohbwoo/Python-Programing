2b67cbf04045410899fff6f70ff46fb494eb2086
kohbwoo <kohbwoo@gmail.com>
2021-11-08 18:31:22 +0900

    6장 연습문제 풀이

--- Chapter 6/6.16.py
+++ Chapter 6/6.16.py
@@ -1,9 +1,11 @@
 student_tuple = (("191101", "홍길동", "010-123-45xx"), ("191102", "임꺽정", "010-223-45xx"), ("191103", "장길산", "010-323-45xx"))
+student_dict = {num: name for num, name, tel in student_tuple}
+
+print("학생정보:", student_dict)
+
 while True:
     tmp = input("학번을 입력하세요 :")
     if tmp == "-1": break
-    for i in student_tuple:
-        if i[0] == tmp:
-            ans = i[1]
-    print(tmp, "번 학생은", ans, "입니다.")
+    print(tmp, "번 학생은", student_dict[tmp], "입니다.")
+
 print("프로그램을 종료합니다.")
---
+++ Chapter 6/6.26.py
@@ -0,0 +1,13 @@
+mylist = [(1, 2), (4, 5), (4, 2), (3, 1), (9, 4)]
+
+a = input("두 정수를 입력하세요.").split()
+a = tuple(map(int, a))
+print(a)
+for i in mylist:
+    if a == i:
+        print(mylist.index(a),"번째에", a,"요소가 있습니다.")
+        break
+    if (a[1], a[0]) == i:
+        print(a,"요소는 없지만" ,mylist.index(a[1], a[0]), "번째에", (a[1],a[0]), "요소가 있습니다.")
+        break
+
