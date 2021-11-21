542194d08ffcbd8c6dac67087aecbe450f575c15
kohbwoo <kohbwoo@gmail.com>
2021-11-21 15:46:48 +0900

    8장 연습문제

---
+++ average function & exception.py
@@ -0,0 +1,23 @@
+def average(arg):
+    try:
+        return sum(arg) / len(arg)
+
+    except ZeroDivisionError:
+        raise Exception('empty argument')
+
+    except:
+        raise Exception('bad argument')
+
+
+def test_average(arg):
+    try:
+        print(arg, ': ', end='')
+        print(average(arg))
+
+    except Exception as e:
+        print(e)
+
+
+test_average([1, 2, 3])
+test_average([])
+test_average(["가", "나"])
