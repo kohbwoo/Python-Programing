f379864f4638e9f3b16e657b53e00a9f76b5f286
kohbwoo <kohbwoo@gmail.com>
2021-11-21 15:30:53 +0900

    8장 연습문제

---
+++ Chapter 8/8.8.py
@@ -0,0 +1,11 @@
+import random
+
+Random_List  = ""
+
+
+for i in range(0,10):
+    Random_List = Random_List + (str(random.randint(1,1000))) + " "
+
+f = open('random_numbers.txt', 'w')
+f.write(str(Random_List))
+f.close()
